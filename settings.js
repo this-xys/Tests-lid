b069cb5ea6e5af5cdf5ae154bbb588eb80ce056f
Cyhper <wilkergoyo405@gmail.com>
2026-03-09 16:45:32 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import { watchFile, unwatchFile } from 'fs'
 import { fileURLToPath } from 'url'
 
-global.owner = ['573196588149', '5492916450307', '5216671548329', '573247662531', '51921826291', '50493732693']
+global.owner = ['573196588149', '556796601394', '5216671548329', '573247662531', '51921826291', '50493732693']
 global.botNumber = ''
 
 global.sessionName = 'Sessions/Owner'
